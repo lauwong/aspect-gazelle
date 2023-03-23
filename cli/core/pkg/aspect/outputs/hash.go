3cb13351ecaafce921538a54211224779dadb216
Jason Bedard <jason@aspect.dev>
2023-03-23 10:37:38 -0700

    fix: move bbclientd accelerated outputs command into cli pro (#1709)
    This moves the recently modified `outputs` command from
    https://github.com/aspect-build/silo/commit/52baf43ea108e9e2dc51c75999e6bb79f644b606
    into the cli/pro and reverts the non-pro `outputs` to the previous
    state.
    
    Questions:
    
    1. ~~Where should the cli docs for the pro version go? Should we rename
    the pro version for the time being?~~ They moved to the plugin command
    2. ~~@alexeagle suggested moving the command into the workflows plugin
    but the APIs are a bit different, I don't see things like command flags
    etc. Is this possible or should we keep it as-is?~~ DONE
    3. Is a simple build+test enough to verify this?
    4. ~~Is it worth refactoring to reduce duplication or keep it simple, at
    least for this PR?~~ (done, I think enough)

--- cli/core/pkg/aspect/outputs/hash.go
+++ cli/core/pkg/aspect/outputs/hash.go
@@ -2,24 +2,17 @@ package outputs
 
 import (
 	"bufio"
-	"crypto/sha256"
-	"encoding/base64"
-	"encoding/hex"
-	"errors"
 	"fmt"
 	"io"
 	"os"
-	"path/filepath"
 	"strings"
 
 	"github.com/aspect-build/silo/cli/core/pkg/bazel"
-	"golang.org/x/exp/slices"
 	"golang.org/x/mod/sumdb/dirhash"
-	"golang.org/x/sync/errgroup"
 )
 
-// addExecutableHash appends the exePath to hashFiles entry of the label
-func addExecutableHash(hashFiles map[string][]string, label string, exePath string) {
+// AddExecutableHash appends the exePath to hashFiles entry of the label
+func AddExecutableHash(hashFiles map[string][]string, label string, exePath string) {
 	_, err := os.Stat(exePath)
 	if os.IsNotExist(err) {
 		fmt.Printf("%s output %s is not on disk, did you build it? Skipping...\n", label, exePath)
@@ -29,9 +22,9 @@ func addExecutableHash(hashFiles map[string][]string, label string, exePath stri
 	hashFiles[label] = append(hashFiles[label], exePath)
 }
 
-// addRunfilesHash iterates through the runfiles entries of the manifest, appending all files
+// AddRunfilesHash iterates through the runfiles entries of the manifest, appending all files
 // contained (or files inside directories) to the hashFiles entry of the label
-func addRunfilesHash(hashFiles map[string][]string, label string, manifestPath string) error {
+func AddRunfilesHash(hashFiles map[string][]string, label string, manifestPath string) error {
 	_, err := os.Stat(manifestPath)
 	if os.IsNotExist(err) {
 		fmt.Printf("%s manifest %s is not on disk, did you build it? Skipping...\n", label, manifestPath)
@@ -73,196 +66,30 @@ func addRunfilesHash(hashFiles map[string][]string, label string, manifestPath s
 	return nil
 }
 
-// Resolve symlinks and directories to find extra outs
-// that can later be hashed.
-//
-// map from Label to the files/directories which should be hashed
-func resolveOuts(outs []bazel.Output) (map[string][]string, error) {
+func printExecutableHashes(outs []bazel.Output) (map[string]string, error) {
+	// map from Label to the files/directories which should be hashed
 	hashFiles := make(map[string][]string)
 
 	for _, a := range outs {
 		if a.Mnemonic == "ExecutableSymlink" {
-			// NB: This stats the file.
-			// TODO: We could probably optimize that away.
-			addExecutableHash(hashFiles, a.Label, a.Path)
+			AddExecutableHash(hashFiles, a.Label, a.Path)
 		} else if a.Mnemonic == "SourceSymlinkManifest" {
-			// NB: This must access and stat the files to see if they are directories
-			// TODO: An optimization could compare them to the output state, maybe.
-			if err := addRunfilesHash(hashFiles, a.Label, a.Path); err != nil {
+			if err := AddRunfilesHash(hashFiles, a.Label, a.Path); err != nil {
 				return nil, err
 			}
 		}
-		// Other Mnemonics are discarded.
 	}
 
-	return hashFiles, nil
-}
-
-// Split the `hashFiles` map into two,
-// one that contains source files and symlinks that point to source files
-// and another that contains generated files and symlinks that point to them.
-func splitHashFiles(hashFiles map[string][]string) (sourceFiles, outputFiles map[string][]string, err error) {
-	sourceFiles = make(map[string][]string)
-	outputFiles = make(map[string][]string)
-
-	for label, files := range hashFiles {
-		for _, file := range files {
-			resolved, err := filepath.EvalSymlinks(file)
-			if err != nil {
-				return nil, nil, fmt.Errorf("could not resolve path: %w\n", err)
-			}
-
-			// TODO: make this more robust,
-			// if we have the output base we can perform a full prefix match.
-			// If we elect to find the output base in the main program.
-			// We can plumb the real bazel-out in here.
-			// TODO: And consider cross-platform filepath comparisons.
-			if strings.Contains(resolved, "/bazel-out/") {
-				outputFiles[label] = append(outputFiles[label], file)
-			} else {
-				sourceFiles[label] = append(sourceFiles[label], file)
-			}
-		}
+	osOpen := func(name string) (io.ReadCloser, error) {
+		return os.Open(name)
 	}
-
-	return
-}
-
-// Equivalent to the per-file hashing from `dirhash.Hash1`
-// The hash and filepath can later be combined to calculate the "h1:" directory hash.
-func hashFile(file string) (string, error) {
-	hasher := sha256.New()
-	f, err := os.Open(file)
-	if err != nil {
-		return "", fmt.Errorf("failed to hash file: %w", err)
-	}
-	defer f.Close()
-
-	if _, err := io.Copy(hasher, f); err != nil {
-		return "", fmt.Errorf("failed to hash file: %w", err)
-	}
-
-	return hex.EncodeToString(hasher.Sum(nil)), nil
-}
-
-// Print the hashes for executable targets
-// This traverses the runfile tree for all (runtime) dependencies,
-// if `bbclientdStatePath` is given it uses `bb_clientd`'s
-// persistent output state to avoid file operations.
-func printExecutableHashes(outs []bazel.Output, bbclientdStatePath string, maximumStateFileSizeBytes int64) (map[string]string, error) {
-
 	result := make(map[string]string)
-	resultHashes := make(map[string][][]string)
-
-	hashFiles, err := resolveOuts(outs)
-	if err != nil {
-		return nil, err
-	}
-
-	// All source files and generated files if there is no bbclientd state file.
-	var readFileContentHashFiles map[string][]string
-
-	// The next two blocks/loops iterate over the files in the two maps
-	// and compute their individual hashes.
-	// They will write into the same result map,
-	// and the resulting overall hash can be calculated afterwards.
-
-	// Output files can be optimized.
-	//
-	// TODO: We could check whether the inner arrays of the `checkOutputStateHashFiles`
-	// are empty and avoid reading the state file entirely.
-	if bbclientdStatePath != "" {
-		// Generated files has their hashes in the bbclientd state file.
-		var checkOutputStateHashFiles map[string][]string
-		readFileContentHashFiles, checkOutputStateHashFiles, err = splitHashFiles(hashFiles)
+	for label, files := range hashFiles {
+		overallhash, err := dirhash.Hash1(files, osOpen)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("failed to compute runfiles hash for manifest: %w\n", err)
 		}
-
-		reader, err := NewStateFileReader(bbclientdStatePath, maximumStateFileSizeBytes)
-		if err != nil {
-			return nil, err
-		}
-		defer reader.Close()
-
-		for label, paths := range checkOutputStateHashFiles {
-			// NB: This reads once per label.
-			// It would be better to flatten the dict and call `Lookup` once
-			// and then rebuild the dictionary form.
-			hashes, missing, err := reader.Lookup(paths)
-			if err != nil {
-				return nil, err
-			}
-			if len(missing) > 0 {
-				return nil, fmt.Errorf("unable to find hashes for requested artifacts: %v, did you build with Remote Output Service and bb-clientd?", missing)
-			}
-
-			hashStrings := make([][]string, 0, len(hashes))
-			for path, hash := range hashes {
-				hashStrings = append(hashStrings, []string{path, hash})
-			}
-
-			resultHashes[label] = append(resultHashes[label], hashStrings...)
-		}
-	} else {
-		readFileContentHashFiles = hashFiles
+		result[label] = overallhash
 	}
-
-	// All source files must be read from disk, and build artifacts without Remote Output Service.
-	for label, files := range readFileContentHashFiles {
-		hashStrings := make([][]string, len(files))
-
-		g := new(errgroup.Group)
-		for i, file := range files {
-			i, file := i, file
-			g.Go(func() error {
-				fileHash, err := hashFile(file)
-				if err != nil {
-					return err
-				}
-
-				hashStrings[i] = []string{file, fileHash}
-				return nil
-			})
-		}
-		err := g.Wait()
-		if err != nil {
-			return nil, err
-		}
-		resultHashes[label] = append(resultHashes[label], hashStrings...)
-	}
-
-	// Calculate the directory hash for the results.
-	for label, hashStrings := range resultHashes {
-		overallHash, err := hash1(hashStrings)
-		if err != nil {
-			return nil, err
-		}
-		result[label] = overallHash
-	}
-
 	return result, nil
-}
-
-// Calculate the directory hash for already hashed files.
-// This follows the implementation for `dirhash.Hash1`
-// but omits all the file operations.
-//
-// hash1 is the "h1:" directory hash function, using SHA-256.
-func hash1(fileHashes [][]string) (string, error) {
-	h := sha256.New()
-
-	slices.SortFunc(fileHashes, func(a, b []string) bool { return a[0] < b[0] })
-
-	for _, fileHash := range fileHashes {
-		file := fileHash[0]
-		hash := fileHash[1]
-		if strings.Contains(file, "\n") {
-			// NB: Bazel will not create such targets, but it is allowed from a unix-perspective.
-			return "", errors.New("dirhash: filenames with newlines are not supported")
-		}
-		fmt.Fprintf(h, "%s  %s\n", hash, file)
-	}
-
-	return "h1:" + base64.StdEncoding.EncodeToString(h.Sum(nil)), nil
 }
--- cli/core/pkg/aspect/outputs/outputs.go
+++ cli/core/pkg/aspect/outputs/outputs.go
@@ -19,19 +19,11 @@ package outputs
 import (
 	"context"
 	"fmt"
-	"os"
-	"path"
-	"strings"
 
 	"github.com/spf13/cobra"
 
 	"github.com/aspect-build/silo/cli/core/pkg/bazel"
 	"github.com/aspect-build/silo/cli/core/pkg/ioutils"
-)
-
-const (
-	BbClientdStateFlag       = "bb-clientd-output-state-prefix"
-	MaximumStateFileSizeFlag = "bb-clientd-output-state-maximum-size-bytes"
 )
 
 type Outputs struct {
@@ -46,7 +38,7 @@ func New(streams ioutils.Streams, bzl bazel.Bazel) *Outputs {
 	}
 }
 
-func (runner *Outputs) Run(_ context.Context, cmd *cobra.Command, args []string) error {
+func (runner *Outputs) Run(_ context.Context, _ *cobra.Command, args []string) error {
 	if len(args) < 1 {
 		return fmt.Errorf("a label is required as the first argument to aspect outputs")
 	}
@@ -55,36 +47,6 @@ func (runner *Outputs) Run(_ context.Context, cmd *cobra.Command, args []string)
 	if len(args) > 1 {
 		mnemonicFilter = args[1]
 	}
-
-	var bbclientdStateFile string
-	bbclientdStatePrefix, err := cmd.Flags().GetString(BbClientdStateFlag)
-	if err != nil {
-		return fmt.Errorf("cannot parse the bb_clientd state file path prefix flag: %w", err)
-	}
-	maximumStateFileSizeBytes, err := cmd.Flags().GetInt64(MaximumStateFileSizeFlag)
-	if err != nil {
-		return fmt.Errorf("cannot parse the bb_clientd state file maximum size flag: %w", err)
-	}
-
-	if bbclientdStatePrefix != "" {
-		// This can also be done with `bazel-info` if we prefer a sub process.
-		// and the pipe handling for that.
-		bo, err := runner.bzl.AbsPathRelativeToWorkspace("bazel-out")
-		if err != nil {
-			return err
-		}
-		bazelOut, err := os.Readlink(bo)
-		if err != nil {
-			return err
-		}
-		parts := strings.Split(bazelOut, "/")
-		// Index from the end to work with non-standard locations.
-		outputBase := parts[len(parts)-4]
-		bbclientdStateFile = path.Join(bbclientdStatePrefix, outputBase)
-	}
-
-	// TODO: To maintain performance this should be aware of the requisite remote execution flags
-	// used by the previous build. As this would drop the analysis cache otherwise.
 	agc, err := runner.bzl.AQuery(query)
 	if err != nil {
 		return err
@@ -94,7 +56,7 @@ func (runner *Outputs) Run(_ context.Context, cmd *cobra.Command, args []string)
 	// Special case pseudo-mnemonic indicating we should compute an overall hash
 	// for any executables in the aquery result
 	if mnemonicFilter == "ExecutableHash" {
-		hashes, err := printExecutableHashes(outs, bbclientdStateFile, maximumStateFileSizeBytes)
+		hashes, err := printExecutableHashes(outs)
 		if err != nil {
 			return err
 		}
