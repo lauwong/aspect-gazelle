0e6bf7a34652d4ebb4a8f7b8229d8b4ccc72e67d
Jason Bedard <jason@aspect.dev>
2024-06-06 18:25:44 -0700

    fix: declare copy_to_bin as empty when srcs attribte is empty (#5654)
    This way it gets deleted by gazelle when there are no `srcs`.
    
    ---
    
    ### Changes are visible to end-users: no
    
    ### Test plan
    
    - Covered by existing test cases

--- cli/pro/gazelle/host/builtin.go
+++ cli/pro/gazelle/host/builtin.go
@@ -1,11 +1,19 @@
 package gazelle
 
-import plugin "github.com/aspect-build/silo/cli/pro/gazelle/host/plugin"
+import (
+	plugin "github.com/aspect-build/silo/cli/pro/gazelle/host/plugin"
+	"github.com/bazelbuild/bazel-gazelle/rule"
+)
 
 var builtinKinds = []plugin.RuleKind{
 	// @aspect_bazel_lib
 	plugin.RuleKind{
 		Name: "copy_to_bin",
 		From: "@aspect_bazel_lib//lib:copy_to_bin.bzl",
+		KindInfo: rule.KindInfo{
+			NonEmptyAttrs: map[string]bool{
+				"srcs": true,
+			},
+		},
 	},
 }
