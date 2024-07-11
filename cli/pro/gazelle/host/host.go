5253e53d6c12713c94be64cb267d2a95cbf9bec8
Jason Bedard <jason@aspect.dev>
2024-07-10 17:51:39 -0700

    refactor(cli): update gazelle language key for starzelle (#5997)
    ### Changes are visible to end-users: no
    
    ### Test plan
    
    - Covered by existing test cases

--- cli/pro/gazelle/host/host.go
+++ cli/pro/gazelle/host/host.go
@@ -20,7 +20,7 @@ import (
 	"github.com/emirpasic/gods/sets/treeset"
 )
 
-const GazelleLanguageName = "AspectConfigure"
+const GazelleLanguageName = "aspect-configure"
 
 // A gazelle
 type GazelleHost struct {
