e2d022cfb974b4d92c408d2d1f105072a3b01515
Alex Eagle <alex@aspect.dev>
2025-01-17 09:00:34 -0800

    refactor(cli): load from rules_shell (#7808)
    As of Bazel 8, sh_* should not be considered as builtin.
    It's just another language with its own ruleset.
    
    I think we might flip https://github.com/bazelbuild/bazel/issues/19455
    as a later step to enforce this on ourselves.
    
    ---
    
    ### Changes are visible to end-users: no
    
    ### Test plan
    
    - Covered by existing test cases

--- cli/pro/gazelle/host/builtin.go
+++ cli/pro/gazelle/host/builtin.go
@@ -7,30 +7,6 @@ import (
 var builtinKinds = []plugin.RuleKind{
 	// Native
 	plugin.RuleKind{
-		Name: "sh_binary",
-		KindInfo: plugin.KindInfo{
-			NonEmptyAttrs:  []string{"srcs"},
-			MergeableAttrs: []string{"srcs"},
-			ResolveAttrs:   []string{"deps"},
-		},
-	},
-	plugin.RuleKind{
-		Name: "sh_library",
-		KindInfo: plugin.KindInfo{
-			NonEmptyAttrs:  []string{"srcs"},
-			MergeableAttrs: []string{"srcs"},
-			ResolveAttrs:   []string{"deps"},
-		},
-	},
-	plugin.RuleKind{
-		Name: "sh_test",
-		KindInfo: plugin.KindInfo{
-			NonEmptyAttrs:  []string{"srcs"},
-			MergeableAttrs: []string{"srcs"},
-			ResolveAttrs:   []string{"deps"},
-		},
-	},
-	plugin.RuleKind{
 		Name: "filegroup",
 		KindInfo: plugin.KindInfo{
 			NonEmptyAttrs:  []string{"srcs"},
