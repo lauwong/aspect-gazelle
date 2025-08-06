lauwong/aspect-gazelle

commit 02357660ba8a1d67269c8899dc3a2b592d203f02

fix(cli): support gazelle incremental updates with orion plugins (#9241)
Gazelle separates the APIs for generating targets (\~`GenerateRules():
[]Rule`) vs declaring what a target exports (\~`Imports(Rule):
[]ImportSpec`).

With starlark plugins these APIs were combined so when you generate a
rule you declare: rule, imports, exports. Then we extract subsets of
that info for the `GenerateRules()` and `Imports()` gazelle APIs.

However when running gazelle on only a subset of a repo
`GenerateRules()` will not be invoked in every directory that
`Imports()` might be called on. This PR manually invokes
`GenerateRules()` in this scenario while keeping the starlark API as-is.

My opinion has always been that the complicated gazelle API is not worth
the rare cases where you get any perf gains from this and I stand by
that still. When there is a perf gain lazy-indexing or watch mode should
be used instead anyway.

### Changes are visible to end-users: no

### Test plan

- Covered by existing test cases
- New test cases added

cli/core/gazelle/host/generate.go
@@ -43,6 +43,9 @@ func (host *GazelleHost) GenerateRules(args gazelleLanguage.GenerateArgs) gazell
 
 	cfg := args.Config.Exts[GazelleLanguageName].(*BUILDConfig)
 
+	// Mark this BUILDConfig as generated since it is having real rules generated.
+	cfg.generated = true
+
 	queryCache := cache.Get(args.Config)
 
 	// Stage 1:

cli/core/gazelle/host/resolver.go
@@ -2,6 +2,8 @@ package gazelle
 
 import (
 	"fmt"
+	"log"
+	"path"
 	"strings"
 	"time"
 
@@ -10,6 +12,7 @@ import (
 	BazelLog "github.com/aspect-build/silo/cli/core/pkg/logger"
 	"github.com/bazelbuild/bazel-gazelle/config"
 	"github.com/bazelbuild/bazel-gazelle/label"
+	gazelleLanguage "github.com/bazelbuild/bazel-gazelle/language"
 	"github.com/bazelbuild/bazel-gazelle/repo"
 	"github.com/bazelbuild/bazel-gazelle/resolve"
 	"github.com/bazelbuild/bazel-gazelle/rule"
@@ -39,12 +42,65 @@ func symbolToImportSpec(symbol plugin.Symbol) resolve.ImportSpec {
 	}
 }
 
+const extGeneratedRules = "__starzelle_generated_rules"
+
+func (re *GazelleHost) importsGenerateRules(c *config.Config, r *rule.Rule, f *rule.File) gazelleLanguage.GenerateResult {
+	BazelLog.Debugf("Imports.GenerateRules(%s): //%s:%s", GazelleLanguageName, f.Pkg, r.Name())
+
+	regularFiles, err := common.GetSourceRegularFiles(f.Pkg)
+	if err != nil {
+		log.Fatalf("Error getting regular files for %s: %v", f.Pkg, err)
+	}
+
+	return re.GenerateRules(gazelleLanguage.GenerateArgs{
+		File:   f,
+		Rel:    f.Pkg,
+		Dir:    path.Join(c.RepoRoot, f.Pkg),
+		Config: c,
+
+		// The RegularFiles are processed by the GazelleHost and passed to plugins.
+		RegularFiles: regularFiles,
+
+		// Subdirs and GenFiles are not used by the GazelleHost, just pass empty slices.
+		Subdirs:  []string{},
+		GenFiles: []string{},
+
+		// OtherEmpty and OtherGen are not used by the GazelleHost, just pass empty slices.
+		OtherEmpty: []*rule.Rule{},
+		OtherGen:   []*rule.Rule{},
+	})
+}
+
 // Determine what rule (r) outputs which can be imported.
 func (re *GazelleHost) Imports(c *config.Config, r *rule.Rule, f *rule.File) []resolve.ImportSpec {
 	BazelLog.Debugf("Imports(%s): //%s:%s", GazelleLanguageName, f.Pkg, r.Name())
 
+	cfg := c.Exts[GazelleLanguageName].(*BUILDConfig)
+
+	// If GenerateRules() was not run by gazelle
+	if !cfg.generated {
+		// With GazelleHost the Imports() functionality is provided at rule generation time, not
+		// a separate Import-extraction phase.
+		//
+		// When running partial generations this means we must manually invoke GenerateRules() if it
+		// was not invoked by gazelle as part of the partial run.
+		if c.Exts[extGeneratedRules] == nil {
+			c.Exts[extGeneratedRules] = re.importsGenerateRules(c, r, f)
+		}
+
+		// Find this rule in the host-generated rules
+		genResult := c.Exts[extGeneratedRules].(gazelleLanguage.GenerateResult)
+		for _, genRule := range genResult.Gen {
+			if genRule.Kind() == r.Kind() && genRule.Name() == r.Name() {
+				r = genRule
+				break
+			}
+		}
+	}
+
 	targetDeclarationAttr := r.PrivateAttr(targetDeclarationKey)
 	if targetDeclarationAttr == nil {
+		// This rule was not generated by this gazelle extension.
 		return nil
 	}
 

cli/core/gazelle/host/tests/starzelle/incremental-0/WORKSPACE
@@ -1,0 +1,1 @@
+workspace(name = "incremental-0")

cli/core/gazelle/host/tests/starzelle/incremental-0/a/BUILD.in
@@ -1,0 +1,8 @@
+load("@aspect_rules_js//js:defs.bzl", "js_library")
+
+# a/index.js has a new import, but is not effected due to the test args
+
+js_library(
+    name = "index_lib",
+    srcs = ["index.js"],
+)

cli/core/gazelle/host/tests/starzelle/incremental-0/a/BUILD.out
@@ -1,0 +1,8 @@
+load("@aspect_rules_js//js:defs.bzl", "js_library")
+
+# a/index.js has a new import, but is not effected due to the test args
+
+js_library(
+    name = "index_lib",
+    srcs = ["index.js"],
+)

cli/core/gazelle/host/tests/starzelle/incremental-0/a/index.js
@@ -1,0 +1,1 @@
+import 'b/index.js';

cli/core/gazelle/host/tests/starzelle/incremental-0/b/BUILD.in
@@ -1,0 +1,1 @@
+# b has not been generated, but is not effected due to the test args

cli/core/gazelle/host/tests/starzelle/incremental-0/b/BUILD.out
@@ -1,0 +1,1 @@
+# b has not been generated, but is not effected due to the test args

commit 634b9e5a887f1d6551d43fe815cbf5d8eb401c05

fix(cli): incremental updates in orion plugins generating multiple rules (#9247)
Silly mistake in
https://github.com/aspect-build/silo/commit/02357660ba8a1d67269c8899dc3a2b592d203f02
which caused only the first orion-generated target to be processed.

### Changes are visible to end-users: no

### Test plan

- Covered by existing test cases
- New test cases added

cli/core/gazelle/host/generate.go
@@ -46,6 +46,10 @@ func (host *GazelleHost) GenerateRules(args gazelleLanguage.GenerateArgs) gazell
 	// Mark this BUILDConfig as generated since it is having real rules generated.
 	cfg.generated = true
 
+	return host.generateRules(cfg, args)
+}
+
+func (host *GazelleHost) generateRules(cfg *BUILDConfig, args gazelleLanguage.GenerateArgs) gazelleLanguage.GenerateResult {
 	queryCache := cache.Get(args.Config)
 
 	// Stage 1:

cli/core/gazelle/host/resolver.go
@@ -44,7 +44,7 @@ func symbolToImportSpec(symbol plugin.Symbol) resolve.ImportSpec {
 
 const extGeneratedRules = "__starzelle_generated_rules"
 
-func (re *GazelleHost) importsGenerateRules(c *config.Config, r *rule.Rule, f *rule.File) gazelleLanguage.GenerateResult {
+func (re *GazelleHost) importsGenerateRules(cfg *BUILDConfig, c *config.Config, r *rule.Rule, f *rule.File) gazelleLanguage.GenerateResult {
 	BazelLog.Debugf("Imports.GenerateRules(%s): //%s:%s", GazelleLanguageName, f.Pkg, r.Name())
 
 	regularFiles, err := common.GetSourceRegularFiles(f.Pkg)
@@ -52,7 +52,7 @@ func (re *GazelleHost) importsGenerateRules(c *config.Config, r *rule.Rule, f *r
 		log.Fatalf("Error getting regular files for %s: %v", f.Pkg, err)
 	}
 
-	return re.GenerateRules(gazelleLanguage.GenerateArgs{
+	return re.generateRules(cfg, gazelleLanguage.GenerateArgs{
 		File:   f,
 		Rel:    f.Pkg,
 		Dir:    path.Join(c.RepoRoot, f.Pkg),
@@ -85,7 +85,7 @@ func (re *GazelleHost) Imports(c *config.Config, r *rule.Rule, f *rule.File) []r
 		// When running partial generations this means we must manually invoke GenerateRules() if it
 		// was not invoked by gazelle as part of the partial run.
 		if c.Exts[extGeneratedRules] == nil {
-			c.Exts[extGeneratedRules] = re.importsGenerateRules(c, r, f)
+			c.Exts[extGeneratedRules] = re.importsGenerateRules(cfg, c, r, f)
 		}
 
 		// Find this rule in the host-generated rules

cli/core/gazelle/host/tests/starzelle/incremental-1/WORKSPACE
@@ -1,0 +1,1 @@
+workspace(name = "incremental-1")

cli/core/gazelle/host/tests/starzelle/incremental-1/a/BUILD.in
@@ -1,0 +1,18 @@
+load("@aspect_rules_js//js:defs.bzl", "js_library")
+
+# a/index.js has a new import, but is not effected due to the test args
+
+js_library(
+    name = "index_lib",
+    srcs = ["index.js"],
+)
+
+js_library(
+    name = "a2_lib",
+    srcs = ["a2.js"],
+)
+
+js_library(
+    name = "a3_lib",
+    srcs = ["a3.js"],
+)

cli/core/gazelle/host/tests/starzelle/incremental-1/a/BUILD.out
@@ -1,0 +1,18 @@
+load("@aspect_rules_js//js:defs.bzl", "js_library")
+
+# a/index.js has a new import, but is not effected due to the test args
+
+js_library(
+    name = "index_lib",
+    srcs = ["index.js"],
+)
+
+js_library(
+    name = "a2_lib",
+    srcs = ["a2.js"],
+)
+
+js_library(
+    name = "a3_lib",
+    srcs = ["a3.js"],
+)

cli/core/gazelle/host/tests/starzelle/incremental-1/a/a2.js
@@ -1,0 +1,1 @@
+import 'b/index.js';

cli/core/gazelle/host/tests/starzelle/incremental-1/a/a3.js
@@ -1,0 +1,1 @@
+import 'b/index.js';

cli/core/gazelle/host/tests/starzelle/incremental-1/a/index.js
@@ -1,0 +1,1 @@
+import 'b/index.js';

cli/core/gazelle/host/tests/starzelle/incremental-1/b/BUILD.in
@@ -1,0 +1,1 @@
+# b has not been generated, but is not effected due to the test args

cli/core/gazelle/host/tests/starzelle/incremental-1/b/BUILD.out
@@ -1,0 +1,1 @@
+# b has not been generated, but is not effected due to the test args

cli/core/gazelle/host/tests/starzelle/incremental-1/b/index.js
@@ -1,0 +1,1 @@
+import 'c/index.js';

cli/core/gazelle/host/tests/starzelle/incremental-1/c/BUILD.in
@@ -1,0 +1,6 @@
+load("@aspect_rules_js//js:defs.bzl", "js_library")
+
+js_library(
+    name = "index_lib",
+    srcs = ["index.js"],
+)

cli/core/gazelle/host/tests/starzelle/incremental-1/c/BUILD.out
@@ -1,0 +1,22 @@
+load("@aspect_rules_js//js:defs.bzl", "js_library")
+
+js_library(
+    name = "index_lib",
+    srcs = ["index.js"],
+    deps = [
+        ":c2_lib",
+        "//a:a2_lib",
+        "//a:index_lib",
+        "//c/sub:index_lib",
+    ],
+)
+
+js_library(
+    name = "c2_lib",
+    srcs = ["c2.js"],
+    deps = [
+        "//a:a2_lib",
+        "//a:a3_lib",
+        "//a:index_lib",
+    ],
+)

cli/core/gazelle/host/tests/starzelle/incremental-1/c/c2.js
@@ -1,0 +1,3 @@
+import 'a/index.js';
+import 'a/a2.js';
+import 'a/a3.js';

cli/core/gazelle/host/tests/starzelle/incremental-1/c/index.js
@@ -1,0 +1,4 @@
+import 'a/index.js';
+import 'a/a2.js';
+import 'c/c2.js';
+import 'c/sub/index.js';

cli/core/gazelle/host/tests/starzelle/incremental-1/c/sub/BUILD.in
@@ -1,0 +1,8 @@
+load("@aspect_rules_js//js:defs.bzl", "js_library")
+
+# c/sub/index.js has a new import, but is not effected due to the test args
+
+js_library(
+    name = "index_lib",
+    srcs = ["index.js"],
+)

cli/core/gazelle/host/tests/starzelle/incremental-1/c/sub/BUILD.out
@@ -1,0 +1,8 @@
+load("@aspect_rules_js//js:defs.bzl", "js_library")
+
+# c/sub/index.js has a new import, but is not effected due to the test args
+
+js_library(
+    name = "index_lib",
+    srcs = ["index.js"],
+)

cli/core/gazelle/host/tests/starzelle/incremental-1/c/sub/index.js
@@ -1,0 +1,1 @@
+import 'foobar';

cli/core/gazelle/host/tests/starzelle/incremental-1/js.lang.star
@@ -1,0 +1,49 @@
+aspect.register_rule_kind("js_library", {
+    "From": "@aspect_rules_js//js:defs.bzl",
+    "NonEmptyAttrs": ["srcs"],
+    "MergeableAttrs": ["srcs"],
+    "ResolveAttrs": ["deps"],
+})
+
+def prepare(_):
+    return aspect.PrepareResult(
+        # All source files to be processed
+        sources = [
+            aspect.SourceExtensions(".js"),
+        ],
+        queries = {
+            # A query treated as an array of results
+            "imports": aspect.RegexQuery(
+                filter = "*.js",
+                expression = """import\\s+'(?P<import>[^']+)'""",
+            ),
+        },
+    )
+
+def declare(ctx):
+    for file in ctx.sources:
+        ctx.targets.add(
+            name = file.path[:file.path.rindex(".")] + "_lib",
+            kind = "js_library",
+            attrs = {
+                "srcs": [file.path],
+                "deps": [
+                    aspect.Import(
+                        id = i.captures["import"],
+                        provider = "js",
+                        src = file.path,
+                    )
+                    for i in file.query_results["imports"]
+                ],
+            },
+            symbols = [aspect.Symbol(
+                id = "/".join([ctx.rel, file.path]) if ctx.rel else file.path,
+                provider = "js",
+            )],
+        )
+
+aspect.register_configure_extension(
+    id = "dummy-js",
+    prepare = prepare,
+    declare = declare,
+)